dbd21137198bee1ff6b65d5a9725700aaaa626b0
PamelaCampos485 <pc21485@gmail.com>
2026-05-22 14:48:56 -0300

    Create batalha_naval_aventureiro.c

---
+++ batalha_naval_aventureiro.c
@@ -0,0 +1,44 @@
+//inicio do codigo
+#include <stdio.h>
+
+int main(){
+    //matriz
+    int tabuleiro[10][10];
+    //loops para a matriz fazer o tabuleiro(água)
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+    //Navio na posição horizontal
+    tabuleiro[2][4] = 3;
+    tabuleiro[2][5] = 3;
+    tabuleiro[2][6] = 3;
+
+    //Navio na posição vertical
+    tabuleiro[4][1] = 3;
+    tabuleiro[5][1] = 3;
+    tabuleiro[6][1] = 3;
+
+    //Primeiro navio na posição diagonal
+    tabuleiro[4][4] = 3;
+    tabuleiro[5][5] = 3;
+    tabuleiro[6][6] = 3;
+
+    //Seugundo navio na posição diagonal
+    tabuleiro[1][9] = 3;
+    tabuleiro[2][8] = 3;
+    tabuleiro[3][7] = 3;
+    
+    
+    //Mostra o tabuleiro e as posicões dos navios
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            printf("%d ", tabuleiro[i][j]);
+        }
+    printf("\n");
+    }
+
+    return 0;
+    //FIM DO PROGRAMA
+}
